22e8826ab0f050235544bf83c367f644d2f66eaa
ladarat <ladarat.y@gmail.com>
2015-10-09 12:02:08 +0700

    create durg

--- app/src/main/java/com/day4/enprog/dessertmarker/busevent/BusEventDessrt.java
+++ app/src/main/java/com/day4/enprog/dessertmarker/busevent/BusEventDessrt.java
@@ -1,7 +1,23 @@
 package com.day4.enprog.dessertmarker.busevent;
 
+import com.day4.enprog.dessertmarker.dao.DessertItemDao;
+
 /**
  * Created by Ladarat on 9/10/2558.
  */
 public class BusEventDessrt {
+
+    DessertItemDao dao;
+
+    public BusEventDessrt(DessertItemDao dao) {
+        this.dao = dao;
+    }
+
+    public DessertItemDao getDao() {
+        return dao;
+    }
+
+    public void setDao(DessertItemDao dao) {
+        this.dao = dao;
+    }
 }
